2ceceaa8c28539c460509a42f6ef01308a53f13e
David Freiman <davidfreiman@dfreiman-mac.local>
2015-09-17 22:40:29 -0400

    useless changes

--- models/beerModel.js
+++ models/beerModel.js
@@ -1,6 +1,6 @@
 function getPercentage() {
 	var density = 1.01;
-	var mass = 14000;
+	var mass = 11000;
 	var maxVolume = 20000;
 	var currentVolume  = mass * (1/density);
 	var fillPercentage = currentVolume / maxVolume;
